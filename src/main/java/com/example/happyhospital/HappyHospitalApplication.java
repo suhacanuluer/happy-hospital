f02e0e07859ec6e7bc505664a39e796d16e3af90
Suha Can Uluer <sculuer@gmail.com>
2023-09-11 15:50:08 +0300

    initial commit

---
+++ src/main/java/com/example/happyhospital/HappyHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.happyhospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyHospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyHospitalApplication.class, args);
+    }
+
+}
